740ff2d453cb55cf7c3731132c4a0203666cdda3
ihor <onyshchukihor11@gmail.com>
2020-06-03 15:49:18 +0300

    Updates

---
+++ precache-manifest.a52440607c2302f432c93d9e005c3ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af3e2e368a315641c01198ab69883ce",
+    "url": "/giphy-search/index.html"
+  },
+  {
+    "revision": "eddf94fcc667e8488582",
+    "url": "/giphy-search/static/css/main.37fb7ee6.chunk.css"
+  },
+  {
+    "revision": "e8e358381a1928ce3a2e",
+    "url": "/giphy-search/static/js/2.20c32e04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/giphy-search/static/js/2.20c32e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eddf94fcc667e8488582",
+    "url": "/giphy-search/static/js/main.fe459464.chunk.js"
+  },
+  {
+    "revision": "d648bcc8403da8e92fce",
+    "url": "/giphy-search/static/js/runtime-main.ac76780b.js"
+  }
+]);
